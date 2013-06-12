fa6955fe96a6251f51e1a0663a41ee3819bbd15b
Djordje Lukic <lukic.djordje@gmail.com>
2013-06-12 17:23:10 +0200

    Using stringjs to decode HTML entities

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var request = require('request');
+var S = require('string');
 
 var url = 'http://api.icndb.com/jokes/random';
 
 request({ url: url, 'json': true} , function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body.value.joke);
+      console.log(S(body.value.joke).decodeHTMLEntities().s);
   }
 });
